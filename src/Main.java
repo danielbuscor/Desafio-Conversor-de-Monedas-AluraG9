4b248111ff41ed64a20fff0ee84261b1baa84e6b
danielbuscor <daniel.bus.cor@gmail.com>
2025-11-17 20:23:14 -0400

    Implementada la clase main que ejecuta el programa

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ import com.danielbuscor.challengeconversor.alurag9.operaciones.Logica;
 
 public class Main {
     public static void main(String[] args) {
+        Logica programa = new Logica();
+        programa.iniciarPrograma();
     }
 }
